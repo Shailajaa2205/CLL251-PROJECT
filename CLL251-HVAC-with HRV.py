0f2a63576fe898851df8cfecfe59e359d64ebbad
Shailajaa2205 <162789891+Shailajaa2205@users.noreply.github.com>
2024-05-08 20:53:21 +0530

    Update CLL251-HVAC-with HRV.py

--- CLL251-HVAC-with HRV.py
+++ CLL251-HVAC-with HRV.py
@@ -1,3 +1,4 @@
+# with HRV.
 import matplotlib.pyplot as plt
 
 # Constants
@@ -5,8 +6,8 @@ Cp = 1.005  # specific heat capacity of air in kJ/kg*K
 rho = 1.2   # air density in kg/m³
 V = 200     # house volume in m³
 eta_HRV = 0.5  # efficiency of Heat Recovery Ventilator
-eta_summer = 0.7  # efficiency of air conditioner
-eta_winter = 0.7  # efficiency of furnace
+eta_ac = 0.7  # efficiency of air conditioner
+eta_furnace = 0.7  # efficiency of furnace
 
 # Summer temperature equations
 def summer_temp(hour):
@@ -14,7 +15,8 @@ def summer_temp(hour):
         return -1.07 * hour + 33.42
     elif 6 <= hour < 16:
         return 1.36 * hour + 18.82
-    else:
+    elif
+        16 < hour < 24:
         return -1.07 * hour + 59.14
 
 # Winter temperature equations
@@ -41,7 +43,7 @@ for hour in range(24):
     delta_T_summer = precooled_temp - 21  # temperature difference for cooling
 
     Q_summer = mass * Cp * delta_T_summer  # heat energy for cooling in kJ
-    E_summer = Q_summer / eta_summer  # actual energy consumption of air conditioner in kJ
+    E_summer = Q_summer / eta_ac  # actual energy consumption of air conditioner in kJ
     summer_energy.append(E_summer / 3600)  # convert kJ to kWh and add to list
 
     # Winter simulation
@@ -50,7 +52,7 @@ for hour in range(24):
     delta_T_winter = 26 - preheated_temp  # temperature difference for heating
 
     Q_winter = mass * Cp * delta_T_winter   # heat energy for heating in kJ
-    E_winter = Q_winter / eta_winter   # actual energy consumption of furnace in kJ
+    E_winter = Q_winter / eta_furnace   # actual energy consumption of furnace in kJ
     winter_energy.append(E_winter / 3600)  # convert kJ to kWh and add to list
 
 # Plot hourly energy consumption
@@ -60,7 +62,7 @@ plt.plot(hours, summer_energy, label='Summer')
 plt.plot(hours, winter_energy, label='Winter')
 plt.xlabel('Hour')
 plt.ylabel('Energy Consumption by (air-conditioner/furnace) (kWh)')
-plt.title('Hourly Energy Consumption in a day')
+plt.title('Hourly Energy Consumption in a day with HRV')
 plt.legend()
 plt.grid(True)
 plt.show()
